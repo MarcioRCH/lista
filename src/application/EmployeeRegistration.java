bb245c535e1409be3c78508f15ce3cb0913e464a
MarcioRCH <marciorchsilva@gmail.com>
2022-05-12 22:23:07 -0300

    update

--- src/application/EmployeeRegistration.java
+++ src/application/EmployeeRegistration.java
@@ -1,10 +1,39 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Employee;
+
 public class EmployeeRegistration {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Employee Registration");
+		System.out.print("How many employees will be registered? ");
+		int n = sc.nextInt();
+		System.out.println(" ");
+		
+		List<Employee> list = new ArrayList<>();
+		
+		for(int i=0; i<n; i++) {
+			sc.nextLine();
+			System.out.println("Employee #" + (i + 1) + ":");
+			System.out.print("Id: ");
+			int id = sc.nextInt();
+			sc.nextLine();
+			System.out.print("Name: ");
+			String name = sc.nextLine();
+			System.out.print("Salary: ");
+			double salary = sc.nextDouble();
+			list.add(new Employee(id, name, salary));
+			System.out.println(" ");
+		}
+		
+		sc.close();
 	}
 
 }
--- src/entities/Employee.java
+++ src/entities/Employee.java
@@ -1,5 +1,29 @@
 package entities;
 
 public class Employee {
-
+	
+	private Integer id;
+	private String name;
+	private Double salary;
+	
+	public Employee(int id, String name, double salary) {
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+	public int getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void increaseSalary(double percentage) {
+		percentage =  1 + 1;
+	}
 }
