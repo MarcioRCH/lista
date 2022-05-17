5cf44f25a195ac14e4e505d4a2329564836c7f68
MarcioRCH <marciorchsilva@gmail.com>
2022-05-17 19:55:57 -0300

    Update

--- src2/application/Tax.java
+++ src2/application/Tax.java
@@ -1,10 +1,49 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.TaxPayer;
+
 public class Tax {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		System.out.print("How many taxpayers will you enter? ");
+		int n = sc.nextInt();
+		System.out.println();
+		
+		List<TaxPayer> list = new ArrayList<>();
+		
+		for(int i=0; i<n; i++) {
+			if(i == 0) {
+				System.out.printf("Enter the data of the %dst contributor: \n", (i+1));
+			}else if(i == 1) {
+				System.out.printf("Enter the data of the %dnd contributor: \n", (i+1));
+			}else {
+				System.out.printf("Enter the data of the %drd contributor: \n", (i+1));
+			}
+			System.out.print("Annual income with salary: ");
+			double salaryIncome = sc.nextDouble();
+			System.out.print("Annual income with service provision: ");
+			double servicesIncome = sc.nextDouble();
+			System.out.print("Annual income with capital gain: ");
+			double capitalIncome = sc.nextDouble();
+			System.out.print("Medical expenses: ");
+			double healthSpending = sc.nextDouble();
+			System.out.print("Educational expenses: ");
+			double educationSpending = sc.nextDouble();
+			System.out.println();
+			list.add(new TaxPayer(salaryIncome, servicesIncome, capitalIncome, healthSpending, educationSpending));
+		}
+		for(TaxPayer obj : list) {
+			//int i = 0;
+			System.out.println(obj);
+		}
+		sc.close();
 	}
 
 }
--- src2/entities/TaxPayer.java
+++ src2/entities/TaxPayer.java
@@ -1,5 +1,45 @@
 package entities;
 
 public class TaxPayer {
-
+	
+	private Double salaryIncome;
+	private Double servicesIncome;
+	private Double capitalIncome;
+	private Double healthSpending;
+	private Double educationSpending;
+	
+	public TaxPayer(double salaryIncome, double servicesIncome, double capitalIncome, double healthSpending, double educationSpending) {
+		this.salaryIncome = salaryIncome;
+		this.servicesIncome = servicesIncome;
+		this.capitalIncome = capitalIncome;
+		this.healthSpending = healthSpending;
+		this.educationSpending = educationSpending;
+	}
+	public double getSalaryIncome() {
+		return salaryIncome;
+	}
+	public double getServicesIncome() {
+		return servicesIncome;
+	}
+	public double getCapitalIncome() {
+		return capitalIncome;
+	}
+	public double getHealthSpending() {
+		return healthSpending;
+	}
+	public double getEducationSpending() {
+		return educationSpending;
+	}
+	public static double taxSalary(double salaryIncome) {
+		double annualSalaryIncome = 0;
+		double salary = salaryIncome / 12;
+		if(salary > 0 && salary < 3000.00) {
+			System.out.printf("Salary tax: exempt");
+		}else if(salary >= 3000.00 && salary < 5000.00) {
+			annualSalaryIncome = salaryIncome * 0.10;
+		}else if(salary > 5000.00) {
+			annualSalaryIncome = salaryIncome * 0.20;
+		}
+		return annualSalaryIncome;
+	}
 }
